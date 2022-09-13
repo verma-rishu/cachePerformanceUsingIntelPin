cfb5128fe437287a7360f4f3e602bc884bd87671
Rishu Verma <dev.rishu28@gmail.com>
2022-09-12 22:37:41 -0400

    Updating the utility to accomodate generation of all input matrices

--- utils/randomMatrixGeneratorSG.py
+++ utils/randomMatrixGeneratorSG.py
@@ -29,14 +29,21 @@ def create2DIndexMatrix(n, indexN):
     nonDuplicateMatrix = list(set(map(lambda i: tuple(sorted(i)), matrix)))
     return nonDuplicateMatrix
 
+def create1DGatherMatrix(indexN):
+    matrix = []
+    for i in range(indexN):
+        input = random.randint(1,1000)
+        matrix.append(input)
+    return matrix
 
-def saveMatrix(matrixA, matrixB, filename):
+
+def saveMatrix(inputMatrix, gatherIndexingVector, scatterIndexingVector, gatherMatrix, filename):
     if os.path.exists(filename): 
         os.remove(filename)
     else:   
         print("New file created: ",filename)
     f = open(filename, "w")
-    for i, matrix in enumerate([matrixA, matrixB]):
+    for i, matrix in enumerate([inputMatrix, gatherIndexingVector, scatterIndexingVector, gatherMatrix]):
         if i != 0:
             f.write("\n")
         for line in matrix:
@@ -71,39 +78,60 @@ def main():
     numberOfElements = n*n
     indexN = random.randint(1, numberOfElements/2)
     outpath = args.dump
-    #Create dense matrix
-    matrixA = createRandomMatrix(n)
-    matrixB = create2DIndexMatrix(n,indexN)
-    #print(matrixA)
+
+    #Creating input data matrix, indexing vectors (for scatter and gather both), matrix for gathered data
+    inputMatrix = createRandomMatrix(n)
+    gatherIndexingVector = create2DIndexMatrix(n,indexN)
+    scatterIndexingVector = create2DIndexMatrix(n,len(gatherIndexingVector))
+    gatherMatrix = create1DGatherMatrix(len(scatterIndexingVector))
+    
     #Convert to sparse matrix by replacing value below threshold to 0
     if (args.sparsity):
-        #Replace random x %element to 0 in matrixA
-        matrixA = np.asarray(matrixA)
-        indicesA = np.random.choice(np.arange(matrixA.size), replace=False,
-                           size=int(matrixA.size * (args.sparsity/100)))
-        flatA  = matrixA.flatten()
-        flatA[indicesA] = 0
+        #Replace random x %element to 0 in inputMatrix
+        inputMatrix = np.asarray(inputMatrix)
+        indicesInputMatrix = np.random.choice(np.arange(inputMatrix.size), replace=False,
+                           size=int(inputMatrix.size * (args.sparsity/100)))
+        flatInputMatrix  = inputMatrix.flatten()
+        flatInputMatrix[indicesInputMatrix] = 0
         #Reshape it back to square matrix    
-        flatA = flatA.reshape(n,n)
-        print(flatA)
-        matrixA_csr = sparse.csr_matrix(flatA)
-        #print(matrixA_csr)
-        matrixA = flatA.tolist()
-        csr_Amatrix = "csrA_"+args.dump
-        saveCSRMatrix(matrixA_csr, csr_Amatrix)
+        flatInputMatrix = flatInputMatrix.reshape(n,n)
+        print(flatInputMatrix)
+        inputMatrix_csr = sparse.csr_matrix(flatInputMatrix)
+        inputMatrix = flatInputMatrix.tolist()
+        csr_InputMatrix = "csrInputMatrix_"+args.dump
+        saveCSRMatrix(inputMatrix_csr, csr_InputMatrix)
   
-    #Replace random x %element to 0 in matrixB
-    matrixB = np.asarray(matrixB)
-    flatB  = matrixB.flatten()
-    #Reshape it back to square matrix    
-    flatB = flatB.reshape(len(matrixB),2)
-    print(flatB)
-    #print(matrixA_csr)
-    matrixB_csr = sparse.csr_matrix(flatB)
-    matrixB = flatB.tolist()
-    csr_Bmatrix = "csrB_"+args.dump
-    saveCSRMatrix(matrixB_csr, csr_Bmatrix)
-    saveMatrix(matrixA, matrixB, args.dump)
+    #gatherIndexingVector
+    gatherIndexingVector = np.asarray(gatherIndexingVector)  
+    flatGatherIndexingVector  = gatherIndexingVector.flatten()
+    flatGatherIndexingVector = flatGatherIndexingVector.reshape(len(gatherIndexingVector),2)
+    print(flatGatherIndexingVector)
+    gatherIndexingVector_csr = sparse.csr_matrix(flatGatherIndexingVector)
+    gatherIndexingVector = flatGatherIndexingVector.tolist()
+    csr_GatherIndexingVector= "csrGatherIndexingVector_"+args.dump
+    saveCSRMatrix(gatherIndexingVector_csr, csr_GatherIndexingVector)
+
+    #scatterIndexingVector
+    scatterIndexingVector = np.asarray(scatterIndexingVector)
+    flatScatterIndexingVector  = scatterIndexingVector.flatten()
+    flatScatterIndexingVector = flatScatterIndexingVector.reshape(len(scatterIndexingVector),2)
+    print(flatScatterIndexingVector)
+    scatterIndexingVector_csr = sparse.csr_matrix(flatScatterIndexingVector)
+    scatterIndexingVector = flatScatterIndexingVector.tolist()
+    csr_ScatterIndexingVector = "csrScatterIndexingVector_"+args.dump
+    saveCSRMatrix(scatterIndexingVector_csr, csr_ScatterIndexingVector)
+
+    #gatherMatrix
+    gatherMatrix = np.asarray(gatherMatrix)
+    flatGatherMatrix  = gatherMatrix.flatten()
+    flatGatherMatrix = flatGatherMatrix.reshape(1, len(gatherMatrix))
+    print(flatGatherMatrix)
+    gatherMatrix_csr = sparse.csr_matrix(flatGatherMatrix)
+    gatherMatrix = flatGatherMatrix.tolist()
+    csr_GatherMatrix = "csrGatherMatrix_"+args.dump
+    saveCSRMatrix(gatherMatrix_csr, csr_GatherMatrix)
+
+    saveMatrix(inputMatrix, gatherIndexingVector,scatterIndexingVector, gatherMatrix, args.dump)
 
 if __name__ == '__main__':
     main()
